b0992a1a46dbfe17c0f49877087df40d1f04a331
MBKCHEL <mbkchelofficialdev@gmail.com>
2026-09-03 17:41:33 +0300

    feat: add 3 git info (skeleton)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use git2::Repository;
+
+fn main() -> Result<(), git2::Error> {
+    let repo = match Repository::open_from_env() {
+        Ok(repo) => repo,
+        Err(e) => panic!("failes to init {e}"),
+    };
+
+    let head = repo.head()?;
+    let commit = head.peel_to_commit()?;
+
+    let id = commit.id();
+    let id = format!("{:.7}", id);
+
+    let authors = commit.author();
+    let authors = authors.name().unwrap_or("Unknown");
+
+    let branch = head.shorthand().unwrap_or("HEAD");
+
+    println!("Authors: {}", authors);
+    println!("Head: {} ({})", id, branch);
+    println!("Last commit: {}", commit.message().unwrap_or(""));
+
+    Ok(())
 }
