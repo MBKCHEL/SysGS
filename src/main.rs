d2cf28559673aafd635b016013c59e3c5016852f
MBKCHEL <vasyl.kabanarius@gmail.com>
2026-09-04 09:32:20 +0300

    feat: add print all Authors of project

--- src/main.rs
+++ src/main.rs
@@ -12,12 +12,39 @@ fn main() -> Result<(), git2::Error> {
     let id = commit.id();
     let id = format!("{:.7}", id);
 
-    let authors = commit.author();
-    let authors = authors.name().unwrap_or("Unknown");
-
     let branch = head.shorthand().unwrap_or("HEAD");
 
-    println!("Authors: {}", authors);
+    let mut revwalk = repo.revwalk()?;
+    revwalk.push_head()?;
+
+    let mut author_counts: std::collections::HashMap<String, usize> = std::collections::HashMap::new();
+
+    for oid in revwalk.flatten() {
+        if let Ok(c) = repo.find_commit(oid) {
+            let author = c.author();
+            if author.name().is_ok() {
+                let name = author.name().unwrap().to_string();
+                *author_counts.entry(name).or_insert(0) += 1;
+            }
+        }
+    }
+
+    let mut sorted_authors: Vec<(String, usize)> = author_counts.into_iter().collect();
+    sorted_authors.sort_by(|a, b| b.1.cmp(&a.1));
+
+    let top_names: Vec<String> = sorted_authors.iter().take(5).map(|(name, _)| name.clone()).collect();
+
+    let authors_str = if sorted_authors.len() > 5 {
+        format!(
+            "{} (total {})",
+            top_names.join(", "),
+            sorted_authors.len()
+        )
+    } else {
+        top_names.join(", ")
+    };
+
+    println!("Authors: {}", authors_str);
     println!("Head: {} ({})", id, branch);
     println!("Last commit: {}", commit.message().unwrap_or(""));
 
